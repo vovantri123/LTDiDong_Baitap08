562c89ebfbffa66fa670c2e9fdbe11a62fa1115c
vovantri123 <vovantri204@gmail.com>
2025-03-28 14:58:58 +0700

    Fixed bug bai 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.android.material:material:1.9.0")
-
 
 }
